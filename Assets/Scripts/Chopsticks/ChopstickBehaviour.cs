df9e20ade95106c5f4f127bfd8de8829daa5fb00
Hell-Tractor <1104840448@qq.com>
2022-10-22 22:20:01 +0800

    Add chopsticks prefab

--- Assets/Scripts/Chopsticks/ChopstickBehaviour.cs
+++ Assets/Scripts/Chopsticks/ChopstickBehaviour.cs
@@ -1,20 +1,17 @@
-using System;
-using System.Threading.Tasks;
-using System.Linq;
 using UnityEngine;
 
-class ChopstickBehaviour : MonoBehaviour {
+public class ChopstickBehaviour : MonoBehaviour {
 
     public float CurrentLength { get; private set; }
     private GameObject _lastPart = null;
-    public GameObject[] testParts;
-    public AnimationCurve FetchPositionCurve;
-    public float FetchDuration;
-    public float FetchCheckRadius;
+    public GameObject[] InitialParts;
+
+    private float _sumScaleY;
 
     private void Start() {
         CurrentLength = 0;
-        foreach (var part in testParts) {
+        _sumScaleY = 0;
+        foreach (var part in InitialParts) {
             AddPart(part);
         }
     }
@@ -25,25 +22,7 @@ class ChopstickBehaviour : MonoBehaviour {
         part.transform.localPosition = _getNextPartPosition(part.transform.localScale.y);
         _lastPart = part;
         CurrentLength += part.GetComponent<ChopstickPartBehaviour>().Length;
-    }
-
-    public async void Fetch(Vector2 targetPosition, Action<GameObject> onFetch) {
-        transform.localPosition = new Vector3(targetPosition.x, transform.localPosition.y, transform.localPosition.z);
-        float t = 0;
-        while (t < FetchDuration) {
-            t += Time.deltaTime;
-            float y = FetchPositionCurve.Evaluate(t / FetchDuration) * targetPosition.y;
-            if (y > transform.localPosition.y) {
-                GameObject food = Physics2D.OverlapCircleAll(transform.position, FetchCheckRadius)
-                .Where(c => c.CompareTag("Food"))
-                .OrderBy(collider => {
-                    return Vector2.Distance(collider.transform.position, transform.position);
-                }).FirstOrDefault()?.gameObject;
-                onFetch(food);
-            }
-            transform.localPosition = new Vector3(targetPosition.x, y, transform.localPosition.z);
-            await Task.Yield();
-        }
+        _sumScaleY += part.transform.localScale.y;
     }
 
     private Vector3 _getNextPartPosition(float nextLength) {
@@ -51,5 +30,9 @@ class ChopstickBehaviour : MonoBehaviour {
             return new Vector3(0, 0, 0);
         }
         return _lastPart.transform.localPosition - new Vector3(0, (_lastPart.transform.localScale.y + nextLength) * 0.5f, 0);
+    }
+
+    public float GetSumScale() {
+        return _sumScaleY;
     }
 }
