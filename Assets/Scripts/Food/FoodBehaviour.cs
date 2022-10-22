46aad3c1472638508f6eb23c6539de4747e8ebc7
Hell-Tractor <1104840448@qq.com>
2022-10-22 20:42:01 +0800

    Add random move to food

--- Assets/Scripts/Food/FoodBehaviour.cs
+++ Assets/Scripts/Food/FoodBehaviour.cs
@@ -6,7 +6,17 @@ public class FoodBehaviour : MonoBehaviour
     private float _remainTime;
     public AnimationCurve AlphaCurve;
     public Vector2 InitialSpeed;
+    public float SpeedUpdateInterval;
+    public float SpeedUpdateDuration;
+    private float _speedUpdateRemainTime;
     private Rigidbody2D _rigidbody2D;
+    private SpeedState _currentSpeedState;
+    private Vector2 _targetSpeed;
+
+    private enum SpeedState {
+        Stable,
+        Altering
+    };
 
     // Start is called before the first frame update
     void Start()
@@ -14,6 +24,8 @@ public class FoodBehaviour : MonoBehaviour
         _rigidbody2D = GetComponent<Rigidbody2D>();
         _remainTime = DurationSeconds;
         _rigidbody2D.velocity = InitialSpeed;
+        _speedUpdateRemainTime = SpeedUpdateInterval;
+        _currentSpeedState = SpeedState.Stable;
     }
 
     // Update is called once per frame
@@ -21,10 +33,29 @@ public class FoodBehaviour : MonoBehaviour
     {
         _remainTime -= Time.deltaTime;
         SpriteRenderer sp = GetComponent<SpriteRenderer>();
-        sp.color =new Color(sp.color.r,sp.color.g,sp.color.b, AlphaCurve.Evaluate(1 - _remainTime / DurationSeconds));
+        sp.color =new Color(sp.color.r, sp.color.g, sp.color.b, AlphaCurve.Evaluate(1 - _remainTime / DurationSeconds));
         if (_remainTime < 0)
         {
             Destroy(gameObject);
+        }
+
+        if (_currentSpeedState == SpeedState.Stable) {
+            _speedUpdateRemainTime -= Time.deltaTime;
+            if (_speedUpdateRemainTime < 0) {
+                _currentSpeedState = SpeedState.Altering;
+                _speedUpdateRemainTime = SpeedUpdateDuration;
+                _targetSpeed = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f));
+                _targetSpeed = _targetSpeed.normalized * InitialSpeed.magnitude;
+            }
+        } else if (_currentSpeedState == SpeedState.Altering) {
+            _speedUpdateRemainTime -= Time.deltaTime;
+            if (_speedUpdateRemainTime < 0) {
+                _currentSpeedState = SpeedState.Stable;
+                _speedUpdateRemainTime = SpeedUpdateInterval;
+            } else {
+                float t = 1 - _speedUpdateRemainTime / SpeedUpdateDuration;
+                _rigidbody2D.velocity = Vector2.Lerp(InitialSpeed, _targetSpeed, t);
+            }
         }
     }
 
